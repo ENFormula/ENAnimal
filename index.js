988e76f64042a6d649495c44ff9cf0aac903edf4
efres1990 <e.freires@alumnos.urjc.es>
2018-07-30 21:25:47 +0200

    mongo connection

--- index.js
+++ index.js
@@ -1,13 +1,51 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+var express     = require("express"),
+app             = express(),
+bodyParser      = require("body-parser"),
+methodOverride  = require("method-override"),
+mongoose        = require('mongoose');
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+// Connection to DB
+mongoose.connect('mongodb://hulio:enmivida1@ds020218.mlab.com:20218/hulio', function(err, res) {
+if(err) throw err;
+console.log('Connected to Database');
+});
 
-  var mongoose = require('mongoose');
-  mongoose.connect('mongodb://hulio:enmivida1@ds020218.mlab.com:20218/hulio/dogs');
+// Middlewares
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(methodOverride());
+
+// Import Models and controllers
+var models     = require('./models/dogs')(app, mongoose);
+var TVShowCtrl = require('./controllers/dogs');
+
+// Example Route
+var router = express.Router();
+router.get('/', function(req, res) {
+res.send("Hello world!");
+});
+app.use(router);
+// API routes
+var tvshows = express.Router();
+/*tvshows.get('/tvshows', function(req, res) {
+  res.send("Hello world!"+ TVShowCtrl.findAllTVShows);
+  });
+  app.use(tvshows);*/
+// API routes
+var tvshows = express.Router();
+
+tvshows.route('/tvshows')
+.get(TVShowCtrl.findAllTVShows)
+.post(TVShowCtrl.addTVShow);
+
+tvshows.route('/tvshows/:id')
+.get(TVShowCtrl.findById)
+.put(TVShowCtrl.updateTVShow)
+.delete(TVShowCtrl.deleteTVShow);
+
+app.use('/api', tvshows);
+
+// Start server
+app.listen(3000, function() {
+console.log("Node server running on http://localhost:3000");
+});
