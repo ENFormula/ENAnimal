4aa957f48575db0ad50f9e226ad04ee87ea7cfa3
efres1990 <e.freires@alumnos.urjc.es>
2018-08-03 20:20:28 +0200

    folders

--- controllers/register.js
+++ controllers/register.js
@@ -5,8 +5,9 @@ var TVShow  = mongoose.model('user');
 //POST - /user/register
 exports.createUser = function(req, res) {
    console.log('POST');
-
-   var user = new user(req.body);
+   var user = new user({
+    name:    req.body.name
+});
    user.save(function(err, user) {
     if(err) return res.send(500, err.message);
 res.status(200).jsonp(user);
