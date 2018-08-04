a369ad4a9fe0d891d6df0b8607a445cc588b0960
efres1990 <e.freires@alumnos.urjc.es>
2018-08-04 16:29:38 +0200

    pass control and user register control

--- controllers/register.js
+++ controllers/register.js
@@ -1,5 +1,17 @@
 var mongoose = require('mongoose');
 var user = mongoose.model('user');
+var passwordValidator = require('password-validator');
+var schema = new passwordValidator();
+// Add properties to it
+schema
+    .is()
+    .min(8)
+    .is()
+    .max(100)
+    .has()
+    .digits()
+    .has()
+    .uppercase();
 
 //POST - /user/register
 exports.createUser = function (req, res) {
@@ -17,12 +29,37 @@ exports.createUser = function (req, res) {
         userName: req.body.userName
     });
     console.log(req.body);
-    userObject.save(function (err, userObject) {
+    user.findOne({
+        'email': req.body.email
+    }, function (err, userExits) {
+
         if (err) 
-            return res.send(500, err.message);
-        res
-            .status(200)
-            .jsonp(userObject);
+            return res.status(500).jsonp({"error": "error"});
+        if (!userExits) {
+            if (schema.validate(req.body.password)) {
+                userObject
+                    .save(function (err, userObject) {
+                        if (err) 
+                            return res.send(500, err.message);
+                        res
+                            .status(200)
+                            .jsonp(userObject);
+                    });
+            } else {
+                return res
+                    .status(401)
+                    .jsonp({
+                        "error": "La contraseña debe tener, letras y números, una mayúscula y contener más de 8 ca" +
+                                "rácteres"
+                    });
+
+            }
+        } else {
+            return res
+                .status(409)
+                .jsonp({"error": "Usuario ya registrado con ese correo"});
+
+        }
     });
 
 };
