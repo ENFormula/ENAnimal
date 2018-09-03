78c08b4d3b27c39f44484dcd6ff41e1575abf318
efres1990 <e.freires@alumnos.urjc.es>
2018-09-03 03:10:42 +0200

    test adverts

--- controllers/adverts.js
+++ controllers/adverts.js
@@ -1,9 +1,9 @@
 var mongoose = require('mongoose');
-var Adverts  = mongoose.model('adverts');
+var adverts  = mongoose.model('adverts');
 
 
 exports.findAllAdverts = function(req, res) {
-    Adverts.find(function(err, adverts) {
+    adverts.find(function(err, adverts) {
     if(err) res.send(500, err.message);
  
     console.log('GET /adverts')
