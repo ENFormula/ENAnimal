fb1622f973606268c2667f0fe89f065461867d07
efres1990 <e.freires@alumnos.urjc.es>
2018-09-04 19:22:28 +0200

    advert by username

--- models/adverts.js
+++ models/adverts.js
@@ -2,7 +2,7 @@ exports = module.exports = function(app, mongoose) {
     
         var advert = new mongoose.Schema({
             name: 		{ type: String },
-            username: 		{ type: String }
+            userName: 		{ type: String }
 
         });
     
